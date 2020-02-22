cc513fc6718ffb19796dbb7783e8f15c9a5b23a5
Peter Hunt <Peter92@users.noreply.github.com>
2020-02-22 13:30:40 +0000

    Update quick_html.py

--- quick_html.py
+++ quick_html.py
@@ -1,3 +1,22 @@
+"""Quick and easy way of building HTML code through context managers.
+This is a simple module so there is no validation or special cases.
+
+Example:
+    <html>
+        <head>
+            <title>My Webpage</title>
+            <link href="style.css" rel="stylesheet"/>
+        </head>
+    </html>
+
+    with QuickHtml() as web:
+        with web.html():
+            with web.head():
+                web.title()('My Webpage')
+                web.link(href='style.css', rel='stylesheet')
+"""
+
+
 def reserved(word):
     """Parse for any reserved words.
     This is needed for words such as "class", which is used in html but
@@ -10,131 +29,155 @@ def reserved(word):
 
 
 class Element(object):
-    IndentSpacing = 4
+    __slots__ = ('indent', 'is_context_manager', 'parent', 'text')
 
     def __init__(self, **kwargs):
-        self.indent = ' ' * Element.IndentSpacing * Element.Depth
-        self._context_manager = False
+        """Initialise the class and write the element as if it's closed."""
+
+        self.indent = ' ' * self.parent._indent * self.parent._depth
+        self.text = []
+        self.parent._previous = self
+        self.is_context_manager = False
 
         # Write opening element
         if kwargs:
             params = ' '.join('{}={}'.format(reserved(k), v.__repr__()) for k, v in kwargs.items())
-            Element.Lines.append('{}<{} {}/>'.format(self.indent, self.__class__.__name__, params))
+            self.parent._lines.append('{}<{} {}/>'.format(self.indent, self.__class__.__name__, params))
         else:
-            Element.Lines.append('{}<{}/>'.format(self.indent, self.__class__.__name__))
-    
+            self.parent._lines.append('{}<{}/>'.format(self.indent, self.__class__.__name__))
+
     def __enter__(self):
-        self._context_manager = True
-        Element.Lines[-1] = Element.Lines[-1][:-2] + Element.Lines[-1][-1]  # Swap "/>" for ">"
-        Element.Depth += 1
+        """Modify element to make it open."""
+
+        self.is_context_manager = True
+        self.parent._lines[-1] = self.parent._lines[-1][:-2] + self.parent._lines[-1][-1]  # Swap "/>" for ">"
+        self.parent._depth += 1
+
         return self
 
-    def __exit__(self, *args):
-        # Write text
-        if Element.RawText:
-            lines = len(Element.RawText)
-            text = '<br/>'.join(Element.RawText)
-            Element.RawText = []
-
-            if lines > 1:
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Write any text and a closing element."""
+        
+        if self.text:
+            text = '<br/>'.join(self.text)
+            if len(self.text) > 1:
                 with self.parent.p() as paragraph:
                     paragraph(text)
             else:
-                self.write_text(text)
+                indent = ' ' * (self.parent._indent * self.parent._depth)
+                self.parent._lines.append(indent + text.replace('<br/>', '\n{i}<br/>\n{i}'.format(i=indent)))
 
-        # Write closing element
-        Element.Depth -= 1
-        Element.Lines.append('{}</{}>'.format(self.indent, self.__class__.__name__))
+        self.parent._depth -= 1
+        self.parent._lines.append('{}</{}>'.format(self.indent, self.__class__.__name__))
     
     def __call__(self, text):
         """Add text into the element."""
-        Element.RawText.append(text)
 
-        # Force the functions to correctly apply text
-        if not self._context_manager:
+        self.text.append(text)
+
+        # Force call to correctly apply text
+        if not self.is_context_manager:
             self.__enter__()
             self.__exit__(None, None, None)
 
-    @classmethod
-    def write_text(cls, text):
-        indent = ' ' * (cls.IndentSpacing * Element.Depth)
-        Element.Lines.append(indent + text.replace('<br/>', '\n{i}<br/>\n{i}'.format(i=indent)))
 
+class QuickHtml(object):
+    def __init__(self, indent=2):
+        self._indent = indent
+        self._lines = []
+        self._previous = None
+        self._depth = 0
+        self._cls_cache = {}
+        self._html = None
 
-class WebPage(object):
     def __enter__(self):
-        Element.Lines = []
-        Element.Depth = 0
-        Element.RawText = []
         return self
 
-    def __exit__(self, *args):
-        self.html = '\n'.join(Element.Lines)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Cache the html code."""
+
+        self._html = '\n'.join(self._lines)
 
     def __getattr__(self, attr):
-        return type(attr, (Element,), {'parent': self})
+        """Dynamically generate a class from Element."""
+
+        if attr in self._cls_cache:
+            return self._cls_cache[attr]
+        self._cls_cache[attr] = type(attr, (Element,), {'parent': self})
+        return self._cls_cache[attr]
     
     def __call__(self, text):
-        Element.RawText.append(text)
+        """Add text to the previous element."""
+
+        self._previous.text.append(text)
+
+    def __str__(self):
+        """Display page as html.
+        This will raise an error if called before the generation is finished.
+        """
+
+        if self._html is None:
+            raise RuntimeError('code is not ready to display')
+        return self._html
 
 
 if __name__ == '__main__':
     example = '''<html lang="en">
-        <head>
-            <title>
-                Python Flask Bucket List App
-            </title>
-            <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
-            <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet"/>
-        </head>
-        <body>
-            <div class="container">
-                <div class="header">
-                    <nav>
-                        <ul class="nav nav-pills pull-right">
-                            <li role="presentation" class="active">
-                                <a href="#">
-                                    Home
-                                </a>
-                            </li>
-                            <li role="presentation">
-                                <a href="#">
-                                    Sign In
-                                </a>
-                            </li>
-                            <li role="presentation">
-                                <a href="#">
-                                    Sign Up
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
-                    <h3 class="text-muted">
-                        Python Flask App
-                    </h3>
-                </div>
+    <head>
+        <title>
+            Python Flask Bucket List App
+        </title>
+        <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <nav>
+                    <ul class="nav nav-pills pull-right">
+                        <li role="presentation" class="active">
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#">
+                                Sign In
+                            </a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#">
+                                Sign Up
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <h3 class="text-muted">
+                    Python Flask App
+                </h3>
             </div>
-        </body>
-    </html>'''
+        </div>
+    </body>
+</html>'''
 
-    with WebPage() as w:
-        with w.html(lang='en'):
-            with w.head():
-                w.title()('Python Flask Bucket List App')
-                w.link(href='http://getbootstrap.com/dist/css/bootstrap.min.css', rel='stylesheet')
-                w.link(href='http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css', rel='stylesheet')
-            with w.body():
-                with w.div(class_='container'):
-                    with w.div(class_='header'):
-                        with w.nav():
-                            with w.ul(class_='nav nav-pills pull-right'):
+    with QuickHtml(indent=4) as web:
+        with web.html(lang='en'):
+            with web.head():
+                web.title()('Python Flask Bucket List App')
+                web.link(href='http://getbootstrap.com/dist/css/bootstrap.min.css', rel='stylesheet')
+                web.link(href='http://getbootstrap.com/examples/jumbotron-narrow/jumbotron-narrow.css', rel='stylesheet')
+            with web.body():
+                with web.div(class_='container'):
+                    with web.div(class_='header'):
+                        with web.nav():
+                            with web.ul(class_='nav nav-pills pull-right'):
                                 for i, item in enumerate(('Home', 'Sign In', 'Sign Up')):
                                     if i:
                                         kwargs = {}
                                     else:
                                         kwargs = {'class_': 'active'}
-                                    with w.li(role='presentation', **kwargs):
-                                        w.a(href='#')(item)
-                        w.h3(class_='text-muted')('Python Flask App')
+                                    with web.li(role='presentation', **kwargs):
+                                        web.a(href='#')(item)
+                        web.h3(class_='text-muted')('Python Flask App')
 
-    assert w.html == example.replace('"', "'")
+    assert str(web) == example.replace('"', "'")
